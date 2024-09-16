821dcce1851bf5de28efb601c0a63c2b4a44ba99
Deklan Brown <dmbrown@mines.edu>
2024-09-16 16:13:56 -0600

    added methods to the LevelSetup class. getMovingPieces is broken

--- src/levelPieces/LevelSetup.java
+++ src/levelPieces/LevelSetup.java
@@ -1,7 +1,32 @@
 package levelPieces;
 
+import java.util.ArrayList;
+
 public class LevelSetup {
 	
+	public LevelSetup() {
+		
+	}
+	
+	public void createLevel(int levelNum) {
+		
+	}
+	
+	public GamePiece[] getBoard() {
+		
+	}
+	
+	public ArrayList<Moveable> getMovingPieces(){
+		
+	}
+	
+	public ArrayList<GamePiece> getInteractingPieces(){
+		
+	}
+	
+	public int getPlayerStartLoc(){
+		return 2;
+	}
 	
 	public static void main(String[] args) {
 		
