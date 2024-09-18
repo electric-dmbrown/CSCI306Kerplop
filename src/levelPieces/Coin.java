fb61d271d6a597de138c028e39fa47e066ebe058
Deklan Brown <dmbrown@mines.edu>
2024-09-18 08:25:43 -0600

    Added Coin Piece. Move not implemented.

---
+++ src/levelPieces/Coin.java
@@ -0,0 +1,33 @@
+package levelPieces;
+
+import gameEngine.Drawable;
+import gameEngine.InteractionResult;
+import gameEngine.Moveable;
+
+public class Coin extends GamePiece implements Moveable{
+	
+	public Coin(char symbol, String label, int location) {
+		super(symbol, label, location);
+	}
+
+	@Override
+	public InteractionResult interact(Drawable[] gameBoard, int playerLocation) {
+		// TODO Auto-generated method stub
+		if(super.getLocation() == playerLocation) {
+			return InteractionResult.GET_POINT;
+		} else {
+			return InteractionResult.NONE;
+		}
+	}
+	
+	@Override
+	public void move(Drawable[] gameBoard, int playerLocation) {
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
